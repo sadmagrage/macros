7cbb2b43c54219b70e4d9d84fa3649853f07f458
Auto Logon <autologon@CSED.LAB>
2023-03-17 21:28:27 -0300

    on class

---
+++ js/fetchData.js
@@ -0,0 +1,8 @@
+export const fetchData = async () => {
+    const data = await fetch('https://tryingrailway-production.up.railway.app/db')
+    .then(res => res.json())
+    .then(data => data)
+    .catch((err) => console.log(err))
+    const response = await data;
+    return response
+};
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-// import number from "./displayComida.js";
+import { fetchData } from "./fetchData.js";
+
+const DATA = await fetchData();
 
 const BODY = document.querySelector("body");
 const HEADER = document.querySelector('#header');
@@ -13,6 +15,7 @@ const SUGEST = document.querySelector("#sugest");
 const LEFT_ICON = document.querySelector("#leftIcon");
 const RIGHT_ICON = document.querySelector("#rightIcon");
 
+
 LEFT_ICON.addEventListener("click", () => {
     NAV.innerHTML = 'sdjiiasj';
     getComputedStyle(NAV).display != 'flex' ? NAV.style.display = 'flex' : NAV.style.display = 'none';
@@ -24,14 +27,66 @@ RIGHT_ICON.addEventListener("click", () => {
     getComputedStyle(RES).display != 'flex' ? RES.style.display = 'flex' : RES.style.display = 'none';  
 });
 
-DIV_STATS.style.backgroundColor = "purple";
+NAV.innerHTML = `
+<div  class="navitem">
+    <img src="https://cf.shopee.com.br/file/cbf20b122e2574ab47a61d37b05e50ba" id="imgComida" alt="noImg"/>
+    <div id="statsComida">
+        <h2 class="textComida" id="h2Comida" style="color: white">dasdsa</h2>
+        <p class="textComida" id="textComida">{obj.grama}g</p>
+    </div>
+</div>`;
 
-DIV_STATS.innerHTML += ['Peso', 'Bf', 'Idade', 'Altura', 'Superavit', 'Deficit', 'Supino', 'Agacho', 'Terra', 'Stiff', 'Restante', 'Velocidade', 'Minuto', 'Adicional'].map(item => {
-    return (
+
+RES.innerHTML = `
+<p style="color: white">macros</p>
+<div id="sugest" style="background-color: red;">
+    <div style="background-color: black; color: white; display: flex">
+        <div>
+            <p style="color: white; background-color: red;">{item}</p>
+            <div class="navitem" style="display: flex">
+                <img src="https://cf.shopee.com.br/file/cbf20b122e2574ab47a61d37b05e50ba" id="imgComida" alt="noImg"/>
+                <div id="statsComida">
+                    <h2 class="textComida" id="h2Comida" style="color: white">{obj.nome}</h2>
+                    <p class="textComida" id="textComida">{obj.grama}g</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
+
+// DIV_STATS.style.backgroundColor = "purple";
+
+let stats = "";
+let comida = "";
+let data = "";
+
+['Peso', 'Bf', 'Idade', 'Altura', 'Superavit', 'Deficit', 'Supino', 'Agacho', 'Terra', 'Stiff', 'Restante', 'Velocidade', 'Minuto', 'Adicional'].map(item => {
+    stats += `
     <div class="stats">
         <label>${item}</label>
-        <input id placeholder={typeof() === 'undefined'? "" : value } />
-    </div>
-    );
-    // return <Item key={item} name={item} clName='stats' action={(text) => toStats(text, item.toLowerCase())} value={preSet[item.toLowerCase()]} />
+        <input id='${item.toLowerCase()}' placeholder='' />
+    </div>`;
 });
+
+DATA.map(item => {
+    comida += `
+    <div class='comida'>
+        <label>${item.nome}</label>
+        <input class='inputComida' type='number' id='${item.nome.toLowerCase()}' />
+    </div>
+    `;
+});
+
+
+DIV_STATS.innerHTML = stats;
+DIV_COMIDA.innerHTML = comida;
+
+// const comidas = document.querySelectorAll('.inputComida');
+const comidas = document.querySelectorAll('input');
+
+for(let i = 0; i < comidas.length; i++){
+    comidas[i].addEventListener('input', (e) => {
+        console.log('Acessado:' + e.target.id + ' Valor: ' + e.target.value);
+    })
+}
+// console.log(document.querySelectorAll('.comida')[0].childNodes[3].value);
